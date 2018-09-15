47bd7c1e2289f55e85d708c2ba5e0eca423445ff
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-09-15 09:23:40 +0200

    Create Kangaroo.cpp

---
+++ Implementation/Kangaroo.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std ; 
+int main ()
+{
+int x1 , v1 ,x2 , v2 ; 
+cin >> x1 >> v1 >> x2 >> v2 ; 
+
+if (x2 > x1 && v1 < v2) 
+    cout << "NO" ;
+else 
+    cout << "YES" ; 
+}
